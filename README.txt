c2a8bb70322160b112fae0bdcc22c5dd04e7cd9e
imrane bit <imraneqbit@proton.me>
2024-01-23 13:08:54 +0000

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,64 @@
+cpassmann 1.0 by Qubit                cpassmann Reference Guide                 
+
+NAME
+       cpassmannn - c passwords manager
+
+SYNOPSIS
+
+       cpassmann -{option} -{option}...etc
+
+DESCRIPTION
+  Cpassmann is a simple but usefull terminal password manager by Qubit, it not meant to compete with the already available passwords managers but i hope the users find it helpfull ,it basically gives you the ability to protect your password with encryption while having easy access to them , the program needs a usb in order to store the very variable encryption key .
+
+  cpassmann is a enchanted version made with c of a  previous version i made with python , the encryption and decryption process still rely on python because of some complication hat encoutered me while emplementing symetric encryption in c  , the encrypt and decrypt scripts are in the folder named encryption , they rely on the python cryptography library. otherwise the program is totally in c .
+
+  the program have not much of dependencies , you aonly need :
+  *a linux operating sysytem
+  *libsodium cryptography lib in C .
+  *python 3
+
+INSTALATION
+
+
+  you need first to specify to edit the path to the usb in the source code to the path of yours and also the path of your home directory , they are in the head of  the ccode like the following :
+    #include <string.h> 
+    #include <stdio.h>
+    #include <sodium.h>
+    #include <stdlib.h>
+ 
+    char *usbpath = "" ;/*the full path to your usb between inside "" 
+    example :char *usbpath = "/run/media/qubit/myusb" */;
+    char *home = "";/* the full path to your home directory here
+    example : /home/qbit/*/
+
+    write the coresponding info between the "" .
+    then cd to the folder of passmann and run the following commands:
+    chmod +x makecpass
+    ./makecpass 
+    and now you're good to go 
+
+
+USAGE :
+    cpassmann is a command line program that take its arguments that specify the a  ction trought the cmmand line ,the command are as follows:
+
+    SYNOPSIS
+
+       cpassmann -{option} -{option}...etc
+
+  OPTIONS
+      -A : show all the accounts
+      -a : add an account
+      -m : modify account , you should provide a second option regarding what you want to change :
+        -m -u(username) 
+        -m -e(email) 
+        -m -P(password 
+        -m -p(platform)
+
+      -s : search for a account by a keyword 
+          SYNOPSIS : cpassmann -s KEYWORD
+      -d : to delete a account using its number (numbers are shown when printing the accounts ) 
+          SYNOPSIS : cpassmann -d NUMBER
+
+thats all i gess , if you encounter any problem please email me ,or just edit the code yourself if you want and can do it .
+
+SEE YOU , SPACE COWBOY.
---
+++ cpass.c
@@ -0,0 +1,506 @@
+#include <string.h> 
+#include <stdio.h>
+#include <sodium.h>
+#include <stdlib.h>
+
+char *usbpath = "" ;/*the full path to your usb between inside "" 
+    example :char *usbpath = "/run/media/qubit/myusb" */;
+
+char *home = "";/* the full path to your home directory here
+    example : char *home = "/home/qbit/" */
+
+
+//backup file path 
+char *bkfile; 
+
+//file path 
+char *filepath;
+
+//backup key path 
+char *bkkey;
+//keypath 
+char *keypath;
+void defpaths(void){
+  bkfile = malloc(1000*sizeof(char));
+  sprintf(bkfile,"%s.cpassmann/.backup/accounts",home);
+  filepath= malloc(1000*sizeof(char));
+  sprintf(filepath,"%s.cpassmann/accounts",home);
+  bkkey= malloc(1000*sizeof(int));
+  sprintf(bkkey,"%s.cpassmannkey/.backup/key.key",usbpath);
+  keypath = malloc(1000*sizeof(int));
+  sprintf(keypath,"%s.cpassmannkey/key.key",usbpath);
+}
+char *accountline;
+char *c;
+char *randpass(int i){
+  char *password = calloc(i,sizeof(char));
+  c = malloc(sizeof(char)) ;
+  for (;i > 0 ; i--){
+    *c = (char ) (randombytes_uniform(96)+32);
+    strncat(password,c,1);
+  }
+  return password ;
+}
+
+int encrypt(char *filename){
+  char *command = malloc(100*sizeof(char));
+  sprintf(command,"python3 /usr/local/bin/encryption/en.py %s ",filename);
+  system(command);
+  free(command);
+  return 0;
+}
+int decrypt(char *filename ,char *key){
+  char *command = malloc(300*sizeof(char));
+  sprintf(command,"python3 /usr/local/bin/encryption/dec.py %s %s",filename,key);
+  system(command);
+  free(command);
+   return 0;
+}
+char *stcomma(char *text,int num){
+  char *output;
+  extern char *c;
+  c = malloc(2*sizeof(char));
+  switch (num) {
+    case 0:
+      output = malloc(100*sizeof(char));
+      break ;
+    case 1:
+      output = malloc(100*sizeof(char));
+      break;
+    case 2:
+      output = malloc(400*sizeof(char));
+      break;
+    case 3:
+      output = malloc(400*sizeof(char));
+      break;
+
+  }
+  while((*c=*text++) != '\0'){
+    if (*c == ((char) 192)){
+      char *n;
+      *n = ',';
+      strncat(output,n,1);
+    }
+    else {
+      strncat(output,c,1);
+    }
+  }
+  return output;
+
+}
+char *commast(char *text,int num){
+  char *output;
+  extern char *c;
+  c = malloc(2*sizeof(char));
+  switch (num) {
+    case 0:
+      output = malloc(100*sizeof(char));
+      break ;
+    case 1:
+      output = malloc(100*sizeof(char));
+      break;
+    case 2:
+      output = malloc(400*sizeof(char));
+      break;
+    case 3:
+      output = malloc(100*sizeof(char));
+      break;
+
+  }
+  while((*c=*text++) != '\0'){
+    if (*c == ','){
+      char *n = malloc(2*sizeof(char));
+      *n= (char) 192;
+      strncat(output,n,1);
+    }
+    else {
+      strncat(output,c,1);
+    }
+  }
+  return output;
+
+}
+
+char *getcolumn(char *line){
+  char *output = malloc(strlen(line)*sizeof(char));
+  while (*line == ',' ){
+    ++line;
+  }
+  while (*line != ',' &&  *line != '\0'){
+    strncat(output,line,1);
+    ++line;
+  }
+  accountline = line;
+  return output;
+}
+
+void printaccount(int i){
+  printf("%2d:\n",i);
+  printf("   name : %s\n",stcomma(getcolumn(accountline),0));
+  printf("   email : %s\n",stcomma(getcolumn(accountline),1));
+  printf("   password : %s\n",stcomma(getcolumn(accountline),2));
+  printf("   platform : %s\n\n",stcomma(getcolumn(accountline),3));
+}
+
+char *apndclmns(char *name , char *email ,char *password,char *other ){
+  char *o = ",";
+  char *line = malloc((strlen(name)+strlen(email)+strlen(password)+strlen(other)+4)*sizeof(char));
+  strcat(line,name);
+  strcat(line,o);
+  strcat(line,email);
+  strcat(line,o);
+  strcat(line,password);
+  strcat(line,o);
+  strcat(line,other);
+  return line;
+}
+
+void putline(char *line , FILE *file){
+  fputs(line, file);
+  return ;
+}
+char *remend(char *lineh){
+  char *c = malloc(sizeof(char));
+  char *output = malloc(400*sizeof(char));
+  while((*c=*lineh++) != '\0'){
+    if (*c == '\n'){
+     continue;
+    }
+    else {
+      strncat(output,c,1);
+    }
+  }
+  return output;
+
+}
+
+
+
+void showallaccs(char *filename){
+  int i = 0;
+  extern char *accountline;
+  accountline= malloc(1000*sizeof(char));
+  FILE *file = fopen(filename,"r");
+  while (fgets(accountline ,1000,file) != NULL ){
+    printaccount(i++);
+    accountline= calloc(1000,sizeof(char));
+    continue ;
+  }
+  return ;
+}
+
+void getaccount(char *thefile){
+  char *name = malloc (100*sizeof(char));
+  char *email=malloc(100*sizeof(char));
+  char *password = malloc(400*sizeof(char));
+  char *other = malloc(100*sizeof(char));
+  char *commandd = malloc(300*sizeof(char));
+  char *command = malloc(300*sizeof(char));
+  printf("enter the name :");
+  fgets(name,100,stdin);
+  printf("enter the email :");
+  fgets(email,100,stdin);
+  printf("enter the password :");
+  fgets(password,400,stdin);
+  printf("enter the other :");
+  fgets(other,100,stdin);
+  char *acc = malloc(1000*sizeof(char));
+  acc = apndclmns(remend(commast(name,0)),remend(commast(email,2)),remend(commast(password,2)),commast(other,3));
+  sprintf(commandd,"cp %s temp",thefile);
+  system(commandd);
+  FILE *accounts = fopen("temp","a");
+  putline(acc,accounts);
+  free(name);
+  free(email);
+  free(password);
+  free(other);
+  free(acc);
+  sprintf(command,"mv temp %s",thefile);
+  system(command);
+  fclose(accounts);
+  free(commandd);
+  free(command);
+}
+
+void keywordsearch(char *filename,char *keyword){
+  extern char *accountline;
+  accountline= malloc(700*sizeof(char));
+  int i = 0;
+  FILE *file = fopen(filename,"r");
+  while (fgets(accountline ,700,file) != NULL ){
+    if (strstr(accountline,keyword)){
+      printaccount(i++);
+      continue ;
+    }
+    else {
+      ++i;
+      continue;
+    }
+  }
+  fclose(file);
+  return ;
+}
+
+void delaccount(int j ,char *thefile){
+  extern char *accountline;
+  int i = 0;
+  accountline= malloc(700*sizeof(char));
+  char *command= malloc(300*sizeof(char));
+  FILE *tmp = fopen("temp","a");
+  FILE *accs = fopen(thefile,"r");
+  while (fgets(accountline ,700,accs) != NULL ){
+    if (i != j ){
+      fputs(accountline, tmp);
+      ++i;
+      continue ;
+    }
+    else {
+      ++i;
+      continue;
+    }
+  }
+  fclose(accs);
+  fclose(tmp);
+  sprintf(command, "rm %s",thefile);
+  system(command);
+  sprintf(command, "mv temp %s",thefile);
+  system(command);
+  free(command);
+  return ;
+}
+
+int modifyaccount (int accnum , char *option ,char *thefile){
+  extern char *accountline;
+  accountline= malloc(700*sizeof(char));
+  char *command= malloc(300*sizeof(char));
+  char *opt;
+  char *new ;
+  int i = 0;
+  FILE *tmp = fopen("temp","a");
+  FILE *accs = fopen(thefile,"r");
+  if (*option == 'P' ){
+    opt = "password";
+    new = malloc(400*sizeof(char));
+  }
+  else if (*option == 'e'){
+    opt = "email";
+    new = malloc(100*sizeof(char));}
+  else if (*option == 'u'){
+    opt = "user";new = malloc(100*sizeof(char));}
+  else if (*option == 'p'){
+    opt = "ptaform";new = malloc(100*sizeof(char));}
+  else {
+    printf("no valid option\n");
+    return -1;
+  }
+  while (fgets(accountline ,700,accs) != NULL ){
+    if (i != accnum ){
+      fputs(accountline, tmp);
+      ++i;
+      continue ;
+    }
+    else {
+      char *name = malloc(100*sizeof(char));
+      char *email=malloc(100*sizeof(char));
+      char *password = malloc(400*sizeof(char));
+      char *platform = malloc(100*sizeof(char));
+
+      printf("enter the new %s : ",opt);
+      fgets(new,400,stdin);
+      if (*option == 'u'){
+        getcolumn(accountline);
+        email = getcolumn(accountline);
+        password = getcolumn(accountline);
+        platform = getcolumn(accountline);
+        accountline = apndclmns(remend(commast(new,0)),commast(email,2),commast(password,2),commast(platform,3));
+      }
+      else if (*option == 'e'){
+        name = getcolumn(accountline);
+        getcolumn(accountline);
+        password = getcolumn(accountline);
+        platform = getcolumn(accountline);
+        accountline = apndclmns(commast(name,0),remend(commast(new,2)),commast(password,2),commast(platform,3));
+      }
+      else if (*option == 'P'){
+        name = getcolumn(accountline);
+        email = getcolumn(accountline);
+        getcolumn(accountline);
+        platform = getcolumn(accountline);
+        accountline = apndclmns(commast(name,0),commast(email,2),remend(commast(new,2)),commast(platform,3));
+      }
+      else if (*option == 'p'){
+        name = getcolumn(accountline);
+        email = getcolumn(accountline);
+        password = getcolumn(accountline);
+        getcolumn(accountline);
+        strcat(new,"\n");
+        accountline = apndclmns(commast(name,0),commast(email,2),commast(password,2),remend(commast(new,3)));
+      }
+      fputs(accountline, tmp);
+      ++i;
+      free(name);
+      free(email);
+      free(password);
+      free(platform);
+
+      continue;
+    }
+  }
+  fclose(accs);
+  fclose(tmp);
+  sprintf(command, "rm %s",thefile);
+  system(command);
+  sprintf(command, "mv temp %s",thefile);
+  system(command);
+  free(command);
+  return 0 ;
+}
+int genfile(char *filename){
+  FILE *accos = fopen(filename,"r");
+  FILE *accounts = fopen("account","a");
+  char *wtf= malloc(400*sizeof(char));
+  while (fgets(wtf,400,accos)!= NULL){
+    fputs(commast(wtf,2),accounts);
+  }
+  return 0;
+  free(wtf);
+  fclose(accos);
+  fclose(accounts);
+}
+void order(char *filename){
+  FILE *smthng = fopen(filename,"r");
+  FILE *gigity = fopen(filepath,"a");
+  int i = 0;
+  char *wtf= malloc(800*sizeof(char));
+  char *name = malloc(100*sizeof(char));
+  char *email=malloc(100*sizeof(char));
+  char *password = malloc(400*sizeof(char));
+  char *platform = malloc(100*sizeof(char));
+
+
+  while(fgets(name,100,smthng)!= NULL){
+  fgets(email,100,smthng);
+  fgets(password,400,smthng);
+  fgets(platform,100,smthng);
+  wtf = apndclmns(remend(name),remend(email),remend(password),platform);
+  fputs(wtf, gigity);
+    }
+
+
+
+  free(name);
+  free(email);
+  free(password);
+  free(platform);
+  fclose(gigity);
+  fclose(smthng);
+}
+int makebackup(char *bkkey,char *key,char *bkfile ,char *file){
+  FILE *pkey = fopen(key,"r");
+  FILE *pbkkey = fopen(bkkey,"w");
+  char *buff = malloc(80*sizeof(char));
+  while (fgets(buff,80,pkey) != NULL){
+    fputs(buff, pbkkey);
+  }
+  fclose(pkey);
+  fclose(pbkkey);
+  free(buff);
+  FILE *pfile = fopen(file,"r");
+  FILE *pbkfile = fopen(bkfile,"w");
+  char *buffer = malloc(100000000*sizeof(char));
+  while (fgets(buffer,10000000,pfile) != NULL){
+    fputs(buffer, pbkfile);
+  }
+  fclose(pfile);
+  fclose(pbkfile);
+  free(buffer);
+  return 0;
+}
+int getbackup(char *bkkey,char *key,char *bkfile ,char *file){
+
+  FILE *pkey = fopen(key,"w");
+  FILE *pbkkey = fopen(bkkey,"r");
+  char *buff = malloc(80*sizeof(char));
+  while (fgets(buff,80,pbkkey) != NULL){
+    fputs(buff, pkey);
+  }
+  fclose(pkey);
+  fclose(pbkkey);
+  free(buff);
+  FILE *pfile = fopen(file,"w");
+  FILE *pbkfile = fopen(bkfile,"r");
+  char *buffer = malloc(100000000*sizeof(char));
+  while (fgets(buffer,10000000,pbkfile) != NULL){
+    fputs(buffer, pfile);
+  }
+  fclose(pfile);
+  fclose(pbkfile);
+  free(buffer);
+  return 0;
+}
+int main (int argc, char *argv[]){
+  defpaths();
+  int s,a,d,m,A,B,U;
+  U = 3;
+  s = a = d = m = A = B = 0;
+  char *test = malloc(10000000*sizeof(char));
+
+  if (fopen(filepath,"r"))
+    ;
+  else {
+    fopen(filepath,"a");
+  }
+    
+
+  if ((argc > 1 ) ){
+    if (*argv[1] == '-'){
+      if ( *(argv[1]+1)== 'b'){
+        makebackup(bkkey,keypath,bkfile,filepath);
+      }
+      else if ( *(argv[1]+1)== 'g' && *(argv[1]+2)== 'b' ){
+        getbackup(bkkey,keypath,bkfile,filepath);
+      }
+      else {
+        decrypt(filepath,keypath);
+        FILE *decryptiontest = fopen(filepath,"r");
+        while (fgets(test,10000000,decryptiontest) != NULL && U-- >=0){
+          ++B;
+        }
+        fclose(decryptiontest);
+        free(test);
+        if (B != 1){
+          if ( *(argv[1]+1)== 'A'){
+            showallaccs(filepath);
+          }
+          else if ( *(argv[1]+1)== 's'){
+            char *keyword;
+            keywordsearch(filepath,argv[2]);
+          }
+          else if ( *(argv[1]+1)== 'd'){
+            delaccount(atoi(argv[2]),filepath);
+          }
+          else if ( *(argv[1]+1)== 'a'){
+            getaccount(filepath); }
+          else if ( *(argv[1]+1)== 'm'){
+            modifyaccount(atoi(argv[2]),(argv[3]+1),filepath);
+          }
+          encrypt(filepath); 
+          char *command = malloc(300*sizeof(char));
+          sprintf(command,"mv key.key %s",keypath);
+          system(command);
+          free(command);
+        }
+        else {
+        printf("decryption eror\n");
+        return -2;
+
+        }}}
+
+    return 0;
+  }
+  else {
+    return -1 ;
+  }
+
+  free(test);
+}
